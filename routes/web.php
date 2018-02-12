fc596354c34b99926d6772355d19423366155ce3
dani17r <danielrangel17md@hotmail.com>
2018-02-12 09:56:43 -0400

    continuacion del tutorial, modificaciones en la ruta.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,32 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'home';
+});
+
+Route::get('/usuarios', function(){
+    return 'usuarios';
+});
+
+/* Route::get('/usuarios/detalles', function(){
+  return 'mostrando detalles del usuario: '.$_GET['id'];
+}); */
+
+//cambiar el orden de las rutas.
+Route::get('usuarios/nuevo', function(){
+    return 'crear nuevo usuario:';
+});
+
+//usuarios/nuevo != usuarios/'[0-9]+'
+Route::get('/usuarios/{id}', function($id){
+  return "mostrando detalles del usuario: {$id}";
+}); /*->where('id','[0-9]+');('id','\d+') para letras y numeros se pone ('id','\w+')*/
+
+Route::get('/saludos/{name}/{nickname?}', function($name, $nikename = null){ //con el signo ? en el parametro de nikname se indica que es opcional
+  if ($nikename) {
+    return "Tu nombre es {$name}, y tu apodo es {$nikename}";
+  }else {
+    return "Tu nombre es {$name}, y no tienes apodo";
+  }
+
 });
